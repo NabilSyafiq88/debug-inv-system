d2f68ce3a6e3a492c4244051b83c8dee73692958
NabilSyafiq88 <nabilsyafiq.razali@fluke.com>
2024-10-11 08:54:40 +0800

    set admin page - for test cells

--- inventory_system/inventory_system/urls.py
+++ inventory_system/inventory_system/urls.py
@@ -49,6 +49,19 @@ urlpatterns = [
     path('manage_sku/', AdminViews.manage_sku, name="manage_sku"),
     path('manage_failuremode/', AdminViews.manage_failuremode, name="manage_failuremode"),
     path('manage_failuredata/', AdminViews.manage_failuredata, name="manage_failuredata"),
+    
+    path('add_cells/', AdminViews.add_cells, name="add_cells"),
+    path('add_cells_save/', AdminViews.add_cells_save, name="add_cells_save"),
+    #path('edit_cells/', AdminViews.edit_cells, name="edit_cells"),
+    path('edit_cells/<cells_id>', AdminViews.edit_cells, name="edit_cells"),
+    path('edit_cells_save/', AdminViews.edit_cells_save, name="edit_cells_save"),
+    path('delete_cells/<cells_id>/', AdminViews.delete_cells, name="delete_cells"),
+    #path('delete_cell/', AdminViews.delete_cell, name="delete_cell"),
+    path('add_sku/', AdminViews.add_sku, name="add_sku"),
+    path('add_sku_save/', AdminViews.add_sku_save, name="add_sku_save"),
+    path('add_failuremode/', AdminViews.add_failuremode, name="add_failuremode"),
+    path('add_failuredata/', AdminViews.add_failuredata, name="add_failuredata"),
+  
     #path('admin_home/', AdminViews.admin_home, name="admin_home"),
     
     
--- inventory_system/inventoryapp/AdminViews.py
+++ inventory_system/inventoryapp/AdminViews.py
@@ -56,3 +56,90 @@ def manage_failuredata(request):
         "failure_data": failure_data
     }
     return render(request, "admin_template/manage_failuredata_template.html", context) 
+
+def add_cells(request):
+    return render(request, "admin_template/add_cells_template.html")
+  
+def add_cells_save(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method!")
+        return redirect('add_cells')
+    else:
+        cells = request.POST.get('cells_name')
+        print (cells)
+        try:
+            cells_model = cells_Name(cell_Name=cells)
+            cells_model.save()
+            messages.success(request, "Cells Added Successfully!")
+            return redirect('add_cells')
+        except:
+            messages.error(request, "Failed to Add Cells!")
+            return redirect('add_cells')
+          
+def edit_cells(request, cells_id):
+    cells = cells_Name.objects.get(id=cells_id)
+
+    context = {
+        "id": cells_id,
+        "cells": cells
+    }
+    print (cells_id)
+    #print (cells)
+    return render(request, 'admin_template/edit_cells_template.html', context)
+  
+def edit_cells_save(request):
+    if request.method != "POST":
+        HttpResponse("Invalid Method")
+    else:
+        cells_id = request.POST.get('cells_id')
+        cells_name = request.POST.get('cells_name')
+        
+        print(cells_id)
+        print(cells_name)
+  
+
+        try:
+            cells = cells_Name.objects.get(id=cells_id)
+            cells.cell_Name = cells_name
+            cells.save()
+
+            messages.success(request, "Course Updated Successfully.")
+            return redirect('/edit_cells/'+cells_id)
+
+        except:
+            messages.error(request, "Failed to Update Course.")
+            return redirect('/edit_cells/'+cells_id)
+          
+def delete_cells(request, cells_id):
+    cells = cells_Name.objects.get(id=cells_id)
+    try:
+        cells.delete()
+        messages.success(request, "Cells Deleted Successfully.")
+        return redirect('manage_cells')
+    except:
+        messages.error(request, "Failed to Delete Cells.")
+        return redirect('manage_cells')        
+
+def add_sku(request):
+    return render(request, "admin_template/add_sku_template.html")
+  
+def add_sku_save(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method!")
+        return redirect('add_sku')
+    else:
+        sku = request.POST.get('sku')
+        try:
+            sku_model = Sku_Info(sku_name=sku)
+            sku_model.save()
+            messages.success(request, "SKU Added Successfully!")
+            return redirect('add_sku')
+        except:
+            messages.error(request, "Failed to Add SKU!")
+            return redirect('add_sku')
+  
+def add_failuremode(request):
+    return render(request, "admin_template/add_failuremode_template.html")
+  
+def add_failuredata(request):
+    return render(request, "admin_template/add_failuredata_template.html")
